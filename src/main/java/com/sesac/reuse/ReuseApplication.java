fa8d53edbc48830620c2c0ae1c32d76e0e6ae02f
yejin <yejinoh58@gmail.com>
2023-08-28 19:26:42 +0900

    first commit

---
+++ src/main/java/com/sesac/reuse/ReuseApplication.java
@@ -0,0 +1,13 @@
+package com.sesac.reuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReuseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesac/reuse/ReuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesac.reuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReuseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
