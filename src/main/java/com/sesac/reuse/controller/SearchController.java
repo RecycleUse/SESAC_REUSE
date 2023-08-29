b44a11607a81141f3d48e822701f700067e19044
dahee oh <ekgml4122@gmail.com>
2023-08-29 21:02:27 +0900

    검색 실패 페이지 UI 구현

---
+++ src/main/java/com/sesac/reuse/controller/SearchController.java
@@ -0,0 +1,21 @@
+package com.sesac.reuse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 검색 페이지 제공용 컨트롤러
+@Controller
+public class SearchController {
+
+    @GetMapping("/search_fail")
+    public String searchFail() {
+        return "search_fail";
+    }
+
+    @GetMapping("/게시판")
+    public String 게시판() {
+        return "게시판";
+    }
+
+}
