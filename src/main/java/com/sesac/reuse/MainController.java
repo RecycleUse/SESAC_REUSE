c29c4a259abe29706bfcbe1d5b673201a271ae63
iian <dheyw@naver.com>
2023-08-29 19:14:16 +0900

    static2 update

--- src/main/java/com/sesac/reuse/MainController.java
+++ src/main/java/com/sesac/reuse/MainController.java
@@ -1,9 +1,11 @@
-package com.sesac.reuse.controller;
+package com.sesac.reuse;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Log4j2
 public class MainController {
     @GetMapping("/")
     public String index() {
