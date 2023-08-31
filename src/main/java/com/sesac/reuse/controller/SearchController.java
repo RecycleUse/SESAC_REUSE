0a9ba873794b07f3b06f5f6bb9afd4cf937d36bb
dahee oh <ekgml4122@gmail.com>
2023-09-01 03:43:32 +0900

    검색어 입력시 dropdown 창이 생기고 검색 결과 반환. 검색 결과 클릭시 해당 search_detail 창으로 이동

--- src/main/java/com/sesac/reuse/controller/SearchController.java
+++ src/main/java/com/sesac/reuse/controller/SearchController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,21 +22,28 @@ public class SearchController {
     private ItemRepository itemRepository;
 
     @GetMapping("/search")
-    public String searchItem(@RequestParam("itemName") String itemName, Model model) {
+    @ResponseBody // JSON 형식으로 응답을 반환함을 선언
+    public List<Item> searchItem(@RequestParam("itemName") String itemName) {
         List<Item> foundItems = itemRepository.findByItemNameContaining(itemName);
-
-        if (foundItems.isEmpty()) {
-            return "search_fail";
-        } else if (foundItems.size() == 1) {
-            // 검색 결과가 한 개인 경우, 해당 아이템의 상세 페이지로 바로 이동
-            model.addAttribute("item", foundItems.get(0));
-            return "search_detail";
-        } else {
-            // 검색 결과가 여러 개인 경우, 검색 결과 페이지로 이동
-            model.addAttribute("items", foundItems);
-            return "search_success";
-        }
+        return foundItems;
     }
+
+//    @GetMapping("/search")
+//    public String searchItem(@RequestParam("itemName") String itemName, Model model) {
+//        List<Item> foundItems = itemRepository.findByItemNameContaining(itemName);
+//
+//        if (foundItems.isEmpty()) {
+//            return "search_fail";
+//        } else if (foundItems.size() == 1) {
+//            // 검색 결과가 한 개인 경우, 해당 아이템의 상세 페이지로 바로 이동
+//            model.addAttribute("item", foundItems.get(0));
+//            return "search_detail";
+//        } else {
+//            // 검색 결과가 여러 개인 경우, 검색 결과 페이지로 이동
+//            model.addAttribute("items", foundItems);
+//            return "search_success";
+//        }
+//    }
 
     @GetMapping("/item-detail")
     public String itemDetail(@RequestParam("item_id") String itemId, Model model) {
