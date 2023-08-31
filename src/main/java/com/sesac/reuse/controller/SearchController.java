7ad85af782dabab29b79dccc9caca4a85687c863
dahee oh <ekgml4122@gmail.com>
2023-09-01 04:20:00 +0900

    검색 결과가 여러개일 때와 없을 때, search 버튼 누르면 에러나던거 Controller에 맵핑 추가해서 해결

--- src/main/java/com/sesac/reuse/controller/SearchController.java
+++ src/main/java/com/sesac/reuse/controller/SearchController.java
@@ -22,11 +22,43 @@ public class SearchController {
     private ItemRepository itemRepository;
 
     @GetMapping("/search")
-    @ResponseBody // JSON 형식으로 응답을 반환함을 선언
+    @ResponseBody  // JSON 형식으로 응답을 반환함을 선언 (검색 드롭다운 기능에 사용)
     public List<Item> searchItem(@RequestParam("itemName") String itemName) {
         List<Item> foundItems = itemRepository.findByItemNameContaining(itemName);
         return foundItems;
     }
+
+    @GetMapping("/search_success")
+    public String searchSuccess(@RequestParam("itemName") String itemName, Model model) {
+        List<Item> foundItems = itemRepository.findByItemNameContaining(itemName);
+        model.addAttribute("items", foundItems);
+        return "search_success"; // 검색 결과 페이지로 이동
+    }
+
+    @GetMapping("/item-detail")
+    public String itemDetail(@RequestParam("item_id") String itemId, Model model) {
+        Item foundItem = itemRepository.findById(itemId).orElse(null);
+
+        if (foundItem == null) {
+            return "search_fail";  // 검색 실패 페이지로 이동
+        }
+
+        model.addAttribute("item", foundItem);
+        return "search_detail";  // 검색 상세 페이지로 이동
+    }
+
+    @GetMapping("/search_fail")
+    public String searchFail() {
+        return "search_fail"; // 검색 실패 페이지로 이동
+    }
+
+    @GetMapping("/게시판")
+    public String 게시판() {
+        return "게시판";
+    }
+
+}
+
 
 //    @GetMapping("/search")
 //    public String searchItem(@RequestParam("itemName") String itemName, Model model) {
@@ -44,23 +76,3 @@ public class SearchController {
 //            return "search_success";
 //        }
 //    }
-
-    @GetMapping("/item-detail")
-    public String itemDetail(@RequestParam("item_id") String itemId, Model model) {
-        Item foundItem = itemRepository.findById(itemId).orElse(null);
-
-        if (foundItem == null) {
-            return "search_fail";  // 검색 실패 페이지로 이동
-        }
-
-        model.addAttribute("item", foundItem);
-        return "search_detail";  // 검색 상세 페이지로 이동
-    }
-
-
-    @GetMapping("/게시판")
-    public String 게시판() {
-        return "게시판";
-    }
-
-}
