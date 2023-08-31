2e825fbabe0712c47e6701281d3d345af13f0db7
yejin <yejinoh58@gmail.com>
2023-09-01 00:15:55 +0900

    회원가입시 이메일 인증

--- src/main/java/com/sesac/reuse/emailverification/config/MailConfig.java
+++ src/main/java/com/sesac/reuse/emailverification/config/MailConfig.java
@@ -1,5 +1,7 @@
 package com.sesac.reuse.emailverification.config;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,16 +12,21 @@ import java.util.Properties;
 //JvaMailSender 등록
 
 @Configuration
+@Log4j2
 public class MailConfig {
 
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+    @Value("${spring.mail.password}")
+    private String mailPassword;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-
+        log.info("mailUsername={}",mailUsername);
         javaMailSender.setHost("smtp.naver.com"); //메인 도메인 서버 주소
-        javaMailSender.setUsername("아이디");
-        javaMailSender.setPassword("비밀번호");
+        javaMailSender.setUsername(mailUsername);
+        javaMailSender.setPassword(mailPassword);
 
         javaMailSender.setPort(465);
 
@@ -31,11 +38,11 @@ public class MailConfig {
     private Properties getMailProperties() {
         Properties properties = new Properties();
         properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜 설정
-        properties.setProperty("mail.smtp.auth", "true"); // smtp 인증
-        properties.setProperty("mail.smtp.starttls.enable", "true"); // smtp strattles 사용
+        properties.setProperty("mail.smtp.auth", "true"); //1 smtp 인증
+        properties.setProperty("mail.smtp.starttls.enable", "true"); //1 smtp strattles 사용
         properties.setProperty("mail.debug", "true"); // 디버그 사용
-        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com"); // ssl 인증 서버는 smtp.naver.com
-        properties.setProperty("mail.smtp.ssl.enable","true"); // ssl 사용
+        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com"); //1 ssl 인증 서버는 smtp.naver.com
+        properties.setProperty("mail.smtp.ssl.enable","true"); //1 ssl 사용
         return properties;    }
 
 }
