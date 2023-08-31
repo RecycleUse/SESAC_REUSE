ea4565b26cfaeb224b9ee832c8cd9bea91ce00b9
dahee oh <ekgml4122@gmail.com>
2023-09-01 04:24:36 +0900

    search_success.html, search_fail.html, search_detail.html 파일명 변경

--- src/main/java/com/sesac/reuse/controller/SearchController.java
+++ src/main/java/com/sesac/reuse/controller/SearchController.java
@@ -2,17 +2,14 @@ package com.sesac.reuse.controller;
 
 import com.sesac.reuse.model.entity.Item;
 import com.sesac.reuse.repository.ItemRepository;
-import com.sesac.reuse.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-import java.util.Optional;
 
 // 검색 페이지 제공용 컨트롤러
 @Controller
@@ -28,11 +25,11 @@ public class SearchController {
         return foundItems;
     }
 
-    @GetMapping("/search_success")
+    @GetMapping("/search-success")
     public String searchSuccess(@RequestParam("itemName") String itemName, Model model) {
         List<Item> foundItems = itemRepository.findByItemNameContaining(itemName);
         model.addAttribute("items", foundItems);
-        return "search_success"; // 검색 결과 페이지로 이동
+        return "search-success"; // 검색 결과 페이지로 이동
     }
 
     @GetMapping("/item-detail")
@@ -40,16 +37,16 @@ public class SearchController {
         Item foundItem = itemRepository.findById(itemId).orElse(null);
 
         if (foundItem == null) {
-            return "search_fail";  // 검색 실패 페이지로 이동
+            return "search-fail";  // 검색 실패 페이지로 이동
         }
 
         model.addAttribute("item", foundItem);
-        return "search_detail";  // 검색 상세 페이지로 이동
+        return "search-detail";  // 검색 상세 페이지로 이동
     }
 
-    @GetMapping("/search_fail")
+    @GetMapping("/search-fail")
     public String searchFail() {
-        return "search_fail"; // 검색 실패 페이지로 이동
+        return "search-fail"; // 검색 실패 페이지로 이동
     }
 
     @GetMapping("/게시판")
